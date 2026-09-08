1cdd3803028aa219067fbb0e7f125b26370cc83a
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-08 11:19:16 +0600

    explored navigation vai link.

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <header className='px-5 py-5 border-b-2 border-gray-300'>
-                <h3 className='text-2xl font-bold'>Enginner-story</h3>
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <header className="flex justify-between px-5 py-5 border-b-2 border-gray-300">
+        <h3 className="text-2xl font-bold">Enginner-story</h3>
+        <nav className="space-x-3">
+          <Link href={"/about/contact"}>Contact</Link>
+          <Link href={"/login"}>Login</Link>
+          <Link href={"/register"}>Register</Link>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
